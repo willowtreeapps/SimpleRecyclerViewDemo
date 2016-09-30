caaf146c572dcc884c15633931b04a6fe8163bfc
Rich Friedel <rich.friedel@willowtreeapps.com>
2016-09-30 19:35:03 -0400

    Setup the activity with the generated list

--- app/src/main/java/com/willowtreeapps/simplerecyclerviewdemo/MainActivity.java
+++ app/src/main/java/com/willowtreeapps/simplerecyclerviewdemo/MainActivity.java
@@ -2,6 +2,12 @@ package com.willowtreeapps.simplerecyclerviewdemo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SimpleAdapter adapter = new SimpleAdapter(generateSimpleList());
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.simple_recyclerview);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(adapter);
+    }
+
+    private List<SimpleViewModel> generateSimpleList() {
+        List<SimpleViewModel> simpleViewModelList = new ArrayList<>();
+
+        for (int i = 0; i < 100; i++) {
+            simpleViewModelList.add(new SimpleViewModel(String.format(Locale.US, "This is item %d", i)));
+        }
+
+        return simpleViewModelList;
     }
 }
