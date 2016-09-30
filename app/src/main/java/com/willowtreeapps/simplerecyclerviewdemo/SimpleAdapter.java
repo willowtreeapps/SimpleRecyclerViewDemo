e365f5d7de0fd2c4e793995c7a7dac6869a275fa
Rich Friedel <rich.friedel@willowtreeapps.com>
2016-09-30 19:27:52 -0400

    Setup the adapter with the viewholder

--- app/src/main/java/com/willowtreeapps/simplerecyclerviewdemo/SimpleAdapter.java
+++ app/src/main/java/com/willowtreeapps/simplerecyclerviewdemo/SimpleAdapter.java
@@ -1,6 +1,8 @@
 package com.willowtreeapps.simplerecyclerviewdemo;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 
 import java.util.ArrayList;
@@ -18,17 +20,18 @@ public class SimpleAdapter extends RecyclerView.Adapter {
 
     @Override
     public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
-        return null;
+        final View view = LayoutInflater.from(parent.getContext()).inflate(viewType, parent, false);
+        return new SimpleViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
-
+        ((SimpleViewHolder) holder).bindData(models.get(position));
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return models.size();
     }
 
     @Override
