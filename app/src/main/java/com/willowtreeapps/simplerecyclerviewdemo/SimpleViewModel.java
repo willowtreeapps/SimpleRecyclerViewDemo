6959d1134b57242ac012a4e78ed60a7884e801d6
Rich Friedel <rich.friedel@willowtreeapps.com>
2016-09-30 19:07:33 -0400

    Add viewmodel with basic getter/setter

---
+++ app/src/main/java/com/willowtreeapps/simplerecyclerviewdemo/SimpleViewModel.java
@@ -0,0 +1,20 @@
+package com.willowtreeapps.simplerecyclerviewdemo;
+
+import android.support.annotation.NonNull;
+
+public class SimpleViewModel {
+
+    private String simpleText;
+
+    public SimpleViewModel(@NonNull final String simpleText) {
+        setSimpleText(simpleText);
+    }
+
+    public String getSimpleText() {
+        return simpleText;
+    }
+
+    public void setSimpleText(@NonNull final String simpleText) {
+        this.simpleText = simpleText;
+    }
+}
