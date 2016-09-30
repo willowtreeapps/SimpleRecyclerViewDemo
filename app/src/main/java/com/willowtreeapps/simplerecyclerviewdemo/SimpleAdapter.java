874cf738553cb011da6700afe0eda863beb633e7
Rich Friedel <rich.friedel@willowtreeapps.com>
2016-09-30 19:03:29 -0400

    Add adapter with basic overrides

---
+++ app/src/main/java/com/willowtreeapps/simplerecyclerviewdemo/SimpleAdapter.java
@@ -0,0 +1,25 @@
+package com.willowtreeapps.simplerecyclerviewdemo;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.ViewGroup;
+
+public class SimpleAdapter extends RecyclerView.Adapter {
+
+    public SimpleAdapter() {
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(final ViewGroup parent, final int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
